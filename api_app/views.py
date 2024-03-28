93faa608fab6a70815bf7830ecca271f80308f31
AnjasFedo <fedoaf98@gmail.com>
2024-03-28 20:32:58 +0700

    View ViewSet

--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Letter
+from .serializers import LetterSerializer
 
 # Create your views here.
+
+class LetterViewSet(viewsets.ModelViewSet):
+    queryset = Letter.objects.all()
+    serializer_class = LetterSerializer
