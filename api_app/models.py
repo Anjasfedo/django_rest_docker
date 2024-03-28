f14af390d4f81fce5746f0426aeac6f97824025c
AnjasFedo <fedoaf98@gmail.com>
2024-03-28 20:27:04 +0700

    Create Model

--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Letters(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=100000)
+    sender = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    count = models.IntegerField()
+
+    def __str__(self):
+        return self.name
