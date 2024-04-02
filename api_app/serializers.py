4e8afb90c85ae61f8dae7d2594af4eaa9be2c0bb
AnjasFedo <fedoaf98@gmail.com>
2024-04-02 23:47:55 +0700

    Response Relational

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
 from .models import Letter, Author, User, Tag
 
-class LetterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Letter
-        fields = '__all__'
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
+        fields = '__all__'
+
+
+class LetterSerializer(serializers.ModelSerializer):
+    sender = AuthorSerializer()
+    receiver = UserSerializer()
+    tag = TagSerializer(many=True)
+
+    class Meta:
+        model = Letter
         fields = '__all__'
