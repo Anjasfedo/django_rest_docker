ad43372e399b85f676848478fe71804ab4816cf6
AnjasFedo <fedoaf98@gmail.com>
2024-04-21 23:35:41 +0700

    Ordering & Filtering Query Params

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LetterViewSet, AuthorViewSet, UserViewSet, TagViewSet
+from .views import LetterViewSet
 
 router = DefaultRouter()
 router.register(r'letter', LetterViewSet)
-router.register(r'author', AuthorViewSet)
-router.register(r'user', UserViewSet)
-router.register(r'tag', TagViewSet)
+# router.register(r'author', AuthorViewSet)
+# router.register(r'user', UserViewSet)
+# router.register(r'tag', TagViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- api_app/views.py
+++ api_app/views.py
@@ -1,6 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, filters
 from .models import Letter, Author, User, Tag
 from .serializers import LetterSerializer, AuthorSerializer, UserSerializer, TagSerializer
+
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+# from .validations import letter_query_schema
 
 # Create your views here.
 
@@ -8,18 +13,33 @@ from .serializers import LetterSerializer, AuthorSerializer, UserSerializer, Tag
 class LetterViewSet(viewsets.ModelViewSet):
     queryset = Letter.objects.all()
     serializer_class = LetterSerializer
+    
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ('id', 'title', 'date')
+    ordering = ('id',)
+    
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        queryset = Letter.objects.all()
+        title = self.request.query_params.get('title')
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
 
 
-class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+# class AuthorViewSet(viewsets.ModelViewSet):
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
 
-class TagViewSet(viewsets.ModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
+# class TagViewSet(viewsets.ModelViewSet):
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
