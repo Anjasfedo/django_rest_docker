acd2214dbfeaa280c3d6f952c449b78cdd756f4d
AnjasFedo <fedoaf98@gmail.com>
2024-05-22 13:29:26 +0700

    Init// Init Firebase

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Author, Tag, Letter, User
 # Register your models here.
--- api_app/apps.py
+++ api_app/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+import firebase_admin
+from firebase_admin import credentials
+import os
 
 class ApiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api_app'
+
+    def ready(self):
+        if not firebase_admin._apps:
+            cred_path = os.path.join(os.path.dirname(
+                __file__), 'serviceAccountKey.json')
+            cred = credentials.Certificate(cred_path)
+            firebase_admin.initialize_app(cred)
